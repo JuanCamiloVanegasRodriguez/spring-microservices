4da2a9a1e51f5fffdcd060548be540a6cc8ef9a3
Juan Camilo Vanegas R <juanlinkjav@gmail.com>
2024-12-23 17:02:35 -0500

    Added admin service to allow monitoring of the ms

---
+++ admin-service/src/test/java/bored/juan/microservices/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bored.juan.microservices.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
