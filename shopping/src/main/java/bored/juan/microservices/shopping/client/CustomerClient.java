4ff5d83be6f5df6d25c81bd8b7b7a2e3f8b1b90b
Juan Camilo Vanegas R <juanlinkjav@gmail.com>
2024-12-23 11:10:35 -0500

    Update Circuit breaker

--- shopping/src/main/java/bored/juan/microservices/shopping/client/CustomerClient.java
+++ shopping/src/main/java/bored/juan/microservices/shopping/client/CustomerClient.java
@@ -1,14 +1,27 @@
 package bored.juan.microservices.shopping.client;
 
 import bored.juan.microservices.shopping.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customers", fallback = CustomerFallbackFactory.class)
+@FeignClient(name = "customers")
 public interface CustomerClient {
 
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customers", fallbackMethod = "getCustomerFallback")
     ResponseEntity<Customer> getCustomer(@PathVariable Long id);
+
+    default ResponseEntity<Customer> getCustomerFallback(Long id, Throwable throwable) {
+        Customer customer = Customer.builder()
+                .id(id)
+                .firstName("None")
+                .lastName("None")
+                .email("None")
+                .photoUrl("None")
+                .build();
+        return ResponseEntity.ok(customer);
+    }
 }
